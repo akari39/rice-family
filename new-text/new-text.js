997c71ed1b1d2a591d731de5629a199950cbf58c
Akari_Hoshisaki <xiaofan2.rs@icloud.com>
2024-03-06 22:06:46 +1100

    Add align dots to ingredients.

--- new-text/new-text.js
+++ new-text/new-text.js
@@ -1,16 +1,28 @@
-function fillLineWithDash() {
-    const lines = document.getElementsByClassName('ingredient-line');
-    lines.array.forEach(line => {
-        console.log(line.parentElement);
-        var parentWidth = line.parentElement.offsetWidth;
-        console.log(parentWidth);
-        var numChars = Math.floor(parentWidth);
-        line.innerText = '.'.repeat(numChars);
-    });
+String.prototype.pxWidth = function(font) {
+    // re-use canvas object for better performance
+    var canvas = String.prototype.pxWidth.canvas || (String.prototype.pxWidth.canvas = document.createElement("canvas")),
+        context = canvas.getContext("2d"); 
+  
+    font && (context.font = font);
+    var metrics = context.measureText(this);
+  
+    return metrics.width;
+  }
+
+function fillLineWithDot() {
+    const lines = document.getElementsByClassName("ingredient-line");
+    for (const line of lines) {
+        const parentElement = line.parentElement;
+        const parentWidth = parentElement.offsetWidth;
+        const rawExistingText = parentElement.innerText.replaceAll('.', '');
+        const font = getComputedStyle(parentElement).getPropertyValue('font');
+        const existingTextWidth = rawExistingText.pxWidth(font);
+        const dotWidth = ".".pxWidth(font);
+        const numChars = Math.floor((parentWidth - existingTextWidth)/dotWidth);
+        line.innerText = '.'.repeat(numChars - 1);
+    }
 }
 
-// Initial fill
-fillLineWithDash();
-
 // Adjust on window resize
-window.onresize = fillLineWithDash;
+window.onload = fillLineWithDot;
+window.onresize = fillLineWithDot;
