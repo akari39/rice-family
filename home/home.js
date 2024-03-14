8f946583c51d437109c8dc2c238582ae17980ec8
Akari_Hoshisaki <xiaofan2.rs@icloud.com>
2024-03-14 12:12:32 +0800

    FInish the home page.

---
+++ home/home.js
@@ -0,0 +1,36 @@
+let selectedIngredientId = null;
+let dishes = ["Sushi", "Japanese Curry", "Fried Rice", "Risotto", "Bibimbap", "Ochazuke", "Donburi", "Paella", "Rice Pudding", "Mango Sticky Rice"];
+
+function onLoad() {
+    const dishCard = document.getElementById("dish-card");
+    const choicePanel = document.getElementById("choice-panel");
+    const choicePanelChildren = choicePanel.children;
+    for (var i = 0; i < choicePanelChildren.length; i++) {
+        const choicePanelChild = choicePanelChildren[i];
+        const index = i;
+        choicePanelChild.onclick = () => {
+            if (selectedIngredientId !== null) {
+                document.getElementById(selectedIngredientId).style.backgroundColor = "transparent";
+                dishCard.style.visibility = "hidden";
+                dishCard.onclick = null;
+            }
+            if (selectedIngredientId === choicePanelChild.id) {
+                selectedIngredientId = null;
+                document.getElementById("dish-img").src = "../asset/Rice.png";
+            } else {
+                selectedIngredientId = choicePanelChild.id;
+                choicePanelChild.style.backgroundColor = "white";
+                dishCard.style.visibility = "visible";
+                const dishName = dishes[index];
+                dishCard.innerText = dishName;
+                const dishNameWithoutSpaces = dishName.replaceAll(" ", "");
+                document.getElementById("dish-img").src = `../asset/${dishNameWithoutSpaces}.png`;
+                dishCard.onclick = () => {
+                    location.href=`../text-pages/${dishNameWithoutSpaces}.html`;
+                };
+            }
+        };
+    }
+}
+
+window.onload = onLoad;
